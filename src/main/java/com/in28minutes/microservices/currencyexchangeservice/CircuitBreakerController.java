fac67f7603548330f0031891c5830feb8463810a
Jyotsna <jyotsnasingh7346@gmail.com>
2024-02-27 01:34:21 +0530

    CircuitBreakerController

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
